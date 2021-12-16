eec31e0810f2a2cd148a888c82e289053ec28596
Karol Rojas <karol.rojasb@autonoma.edu.co>
2021-12-15 20:34:15 -0500

    separeting backend

--- backend/models/department.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const departmentSchema = mongoose.Schema({
-  id: { type: mongoose.Types.ObjectId },
-  name: { type: String, required: true, unique: true, maxLength: 150 },
-})
-
-module.exports = mongoose.model('departments', departmentSchema)
--- backend/models/role.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const RoleSchema = mongoose.Schema({
-  id: { type: mongoose.Types.ObjectId },
-  name: { type: String, unique: true, required: true, maxLength: 100 },
-  description: { type: String, required: false, maxLength: 350 },
-})
-
-module.exports = mongoose.model('roles', RoleSchema)
