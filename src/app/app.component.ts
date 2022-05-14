3c777f437848f8e705afa4cceca10682ce3427b3
“Daniel <asplabeluam@gmail.com>
2022-05-14 07:41:24 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core'
-import { Subscription } from 'rxjs'
-import { AuthService } from './auth/auth.service'
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-  url: string = '/monitor'
-  subAuht: Subscription
-  title = 'asplabelApp'
-  isAuthenticated = false
-  constructor(private authService: AuthService) {
-  }
+export class AppComponent implements OnInit {
+  url: string = '/monitor';
+  subAuht: Subscription;
+  title = 'asplabelApp';
+  isAuthenticated = false;
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(){
-    this.isAuthenticated = this.authService.getIsAuthenticated()
-    this.subAuht = this.authService.getAuthStatusListener().subscribe(isAuth =>{
-      this.isAuthenticated = isAuth
-    })
-    this.authService.autoAuthUser()
-    this.url = window.location.href
+  ngOnInit() {
+    this.isAuthenticated = this.authService.getIsAuthenticated();
+    this.subAuht = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuth) => {
+        this.isAuthenticated = isAuth;
+      });
+    this.authService.autoAuthUser();
+    this.url = window.location.href;
+    if (this.isAuthenticated) {
+      this.router.navigate(['/monitor']);
+    }
   }
 }
